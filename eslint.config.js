85d5eba0bc4e053be847dbd0a37afc8bb7fac2d6
Quang Sơn Phạm <phamquangson4493@gmail.com>
2025-05-05 15:57:53 +0700

    struct add ESLint Flat Config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,34 @@
-//  @ts-check
+import { tanstackConfig } from "@tanstack/eslint-config";
+import eslintPluginReactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
+import eslintPluginImport from "eslint-plugin-import";
+import prettier from "eslint-config-prettier";
 
-import { tanstackConfig } from '@tanstack/eslint-config'
-
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig({
+    typescript: {
+      tsconfigPath: "./tsconfig.json"
+    },
+    react: true
+  }),
+  {
+    plugins: {
+      "react-refresh": eslintPluginReactRefresh,
+      "simple-import-sort": eslintPluginSimpleImportSort,
+      import: eslintPluginImport
+    },
+    rules: {
+      "react-refresh/only-export-components": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/first": "error",
+      "import/newline-after-import": "error",
+      "import/no-duplicates": "error"
+    }
+  },
+  {
+    rules: {
+      ...prettier.rules
+    }
+  }
+];
